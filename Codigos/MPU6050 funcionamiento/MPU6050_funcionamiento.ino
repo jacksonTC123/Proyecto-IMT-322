9940c6898d4b9e8f72d72c7497c481697b250499
JacksonTC <jason.tarifa.calizaya@gmail.com>
2025-06-05 11:55:20 -0400

    MPU6050 funcionamiento

---
+++ Codigos/MPU6050 funcionamiento/MPU6050_funcionamiento.ino
@@ -0,0 +1,17 @@
+#include "funciones.h"
+
+void setup() {
+  iniciar_comunicacion_serial();
+  configurar_pines();
+  configurar_mpu6050();
+}
+
+void loop() {
+  verificarBoton();
+  if (calibracionSolicitada) {
+    calibrarMPU();
+    calibracionSolicitada = false;
+  }
+  if (!dmpListo) return;
+  leerYMostrarDatosMPU();
+}
